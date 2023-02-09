01b321dadc7e7e1a4f0e8bce0312a9d4eee0dcf2
Carlos <morenocarlos9973@gmail.com>
2023-02-09 17:43:42 -0600

    Create database and connect to banckend

--- web/shopify.js
+++ web/shopify.js
@@ -2,8 +2,15 @@ import { BillingInterval, LATEST_API_VERSION } from "@shopify/shopify-api";
 import { shopifyApp } from "@shopify/shopify-app-express";
 import { SQLiteSessionStorage } from "@shopify/shopify-app-session-storage-sqlite";
 import { restResources } from "@shopify/shopify-api/rest/admin/2023-01";
+import { join } from "path";
+import { QRCodesDB } from "./qr-codes-db";
 
 const DB_PATH = `${process.cwd()}/database.sqlite`;
+const dbFile = join(process.cwd(), "database.sqlite");
+const sessionDb = new SQLiteSessionStorage(dbFile);
+// Initialize SQLite DB
+QRCodesDB.db = sessionDb.db;
+QRCodesDB.init();
 
 // The transactions with Shopify will always be marked as test transactions, unless NODE_ENV is production.
 // See the ensureBilling helper to learn more about billing in this template.
@@ -30,7 +37,7 @@ const shopify = shopifyApp({
     path: "/api/webhooks",
   },
   // This should be replaced with your preferred storage strategy
-  sessionStorage: new SQLiteSessionStorage(DB_PATH),
+  sessionStorage: sessionDb,
 });
 
 export default shopify;
